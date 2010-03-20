21ed137ea6c85a67f3c53db9e39f82892d30e5b3
guillermooo <guilan70@hotmail.com>
2010-03-20 19:46:15 +0100

    refactoring

--- executepscommand.py
+++ executepscommand.py
@@ -6,15 +6,22 @@ import subprocess
 import codecs
 import base64
 
+PATH_TO_THIS_FILE = os.path.dirname(os.path.abspath(__file__))
+
 def getPathToPoShScript():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(BASE_DIR, "psbuff.ps1")
-    # PS_HISTORY_FILE = os.path.join(BASE_DIR, "pshist.txt")
+    return os.path.join(PATH_TO_THIS_FILE, "psbuff.ps1")
+
+def getPathToPoShHistoryDB():
+    return os.path.join(PATH_TO_THIS_FILE, "pshist.txt")
 
 # The PoSh pipeline provided by the user is merged with this template and the
 # resulting PoSh script is passed the text cotained in the selected region in
 # Sublime Text. If many regions exist, they are filtered one after the other.
-PS_SCRIPT_TEMPLATE = """
+#
+# NOTE: PoSh accepts a command as a base64 encoded string, but that way
+# we'd fill up the Windows console's buffer quicker and besides
+# any error info will (apparently) be returned as an XML string
+PoSh_SCRIPT_TEMPLATE = """
 $a = $args[0]
 [void] $(chcp 65001) # we want utf-8 returned from the console!
 # We receive a base64 encoded UTF16LE encoding from the command line.
@@ -30,68 +37,57 @@ class RunExternalPSCommandCommand(sublimeplugin.TextCommand):
     Powershell (PoSh) pipeline. See README.TXT for instructions.
     """
 
-    PS_HISTORY_MAX_LENGTH = 50
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    PS_BUFF_FILE = getPathToPoShScript()
-    PS_HISTORY_FILE = os.path.join(BASE_DIR, "pshist.txt")
+    PoSh_HISTORY_MAX_LENGTH = 50
+    PoSh_BUFF_FILE = getPathToPoShScript()
+    PoSh_HISTORY_FILE = getPathToPoShHistoryDB()
 
     def __init__(self, *args, **kwargs):
-        self.PSHistory = []
-        self.lastFailedCommand = ''
-
-        # If the command history file doesn't exist now, it will be created when
-        # the user chooses to persist the current history for the first time.
-        try:
-            with open(self.PS_HISTORY_FILE, 'r') as f:
-                self.PSHistory = [command[:-1] for command in f.readlines()]
-        except IOError:
-            pass
-
+        self.PSHistory = getPoShSavedHistory()
+        self.lastFailedCommand = ""
         super(sublimeplugin.TextCommand, self).__init__(*args, **kwargs)
 
-    def addToPSHistory(self, command):
+    def _addToPSHistory(self, command):
         if not command in self.PSHistory:
             self.PSHistory.insert(0, command)
-        if len(self.PSHistory) > self.PS_HISTORY_MAX_LENGTH:
+        if len(self.PSHistory) > self.PoSh_HISTORY_MAX_LENGTH:
             self.PSHistory.pop()
 
-    def showPSHistory(self, view):
+    def _showPSHistory(self, view):
         view.window().showQuickPanel('', "runExternalPSCommand", self.PSHistory,
                                         sublime.QUICK_PANEL_MONOSPACE_FONT)
 
-    def run(self, view, args):
-        def onDone(PSCommand):
-            # Intrinsic commands.
-            if PSCommand == '!h':
-                if self.PSHistory:
-                    self.showPSHistory(view)
-                else:
-                    sublime.statusMessage("PoSh command history is empty.")
-                return
-            if PSCommand == '!mkh':
-                with open(self.PS_HISTORY_FILE, 'w') as f:
-                    f.writelines([(x + '\n') for x in self.PSHistory])
-                    sublime.statusMessage("PoSh command history saved.")
-                return
+    def _parseIntrinsicCommands(self, userPoShCmd, view):
+        if userPoShCmd == '!h':
+            if self.PSHistory:
+                self._showPSHistory(view)
+            else:
+                sublime.statusMessage("Powershell command history is empty.")
+            return True
+        if userPoShCmd == '!mkh':
+            with open(self.PoSh_HISTORY_FILE, 'w') as f:
+                cmds = [(cmd + '\n').encode('utf-8') for cmd in self.PSHistory]
+                f.writelines(cmds)
+                sublime.statusMessage("Powershell command history saved.")
+            return True
+        else:
+            return False
 
-            # PoSh accepts a command as a base64 encoded string, but that way
-            # we'd fill up the Windows console's buffer quicker and besides
-            # any error info will (apparently) be returned as an XML string
-            PSScriptContent = (PS_SCRIPT_TEMPLATE % PSCommand)
+    def run(self, view, args):
+        def onDone(userPoShCmd):
+            if self._parseIntrinsicCommands(userPoShCmd, view): return
 
             # UTF8 signature required! If you don't use it, the sky will fall!
             # The Windows console won't interpret correctly a UTF8 encoding
             # without a signature.
-            with codecs.open(self.PS_BUFF_FILE, 'w', 'utf_8_sig') as f:
-                f.write(PSScriptContent)
+            with codecs.open(self.PoSh_BUFF_FILE, 'w', 'utf_8_sig') as f:
+                f.write( (PoSh_SCRIPT_TEMPLATE % userPoShCmd) )
 
             for region in view.sel():
-                text = view.substr(region)
-
+                regionText = view.substr(region)
                 try:
                     startupinfo = subprocess.STARTUPINFO()
                     startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-                    PSOutput, PSErrInfo = subprocess.Popen(getPoShCmdLine(self.PS_BUFF_FILE, text),
+                    PoShOutput, PoShErrInfo = subprocess.Popen(getPoShCmdLine(self.PoSh_BUFF_FILE, regionText),
                                                             shell=False, # TODO: Needed?
                                                             stdout=subprocess.PIPE,
                                                             stderr=subprocess.PIPE,
@@ -100,39 +96,46 @@ class RunExternalPSCommandCommand(sublimeplugin.TextCommand):
                     # We've changed the Windows console's default codepage in the PoSh script
                     # by calling chcp 65001. Therefore, now we need to decode a UTF8 stream
                     # with sinature.
-                    # Note: PSErrInfo still gets encoded in the default codepage.
-                    PSOutput, PSErrInfo = (PSOutput.decode('utf_8_sig'),
-                                          PSErrInfo.decode(getDOSPromptDefaultCodepage()))
+                    # Note: PoShErrInfo still gets encoded in the default codepage.
+                    PoShOutput, PoShErrInfo = ( PoShOutput.decode('utf_8_sig'),
+                                                PoShErrInfo.decode(getDOSPromptDefaultCodepage()), )
 
                 # Catches errors for any OS, not just Windows.
                 except EnvironmentError, e:
                     # TODO: This catches too many errors?
                     sublime.errorMessage("Windows error. Possible causes:\n\n" +
                                           "* Is Powershell in your %PATH%?\n" +
-                                          "* Use Start-Process to start ST from PoSh.\n\n%s" % e)
+                                          "* Use Start-Process to start ST from Powershell.\n\n%s" % e)
                     return
 
-                if PSErrInfo:
-                    print PSErrInfo
+                if PoShErrInfo:
+                    print PoShErrInfo
                     sublime.statusMessage("PowerShell error.")
                     view.window().runCommand("showPanel console")
-                    self.lastFailedCommand = PSCommand
+                    # Later we save the user some typing.
+                    self.lastFailedCommand = userPoShCmd
                     return
-                elif PSOutput:
+                elif PoShOutput:
                     self.lastFailedCommand = ''
-                    # NOTE 1: If you call Popen with shell=True and the length of PSOutput
+                    # NOTE 1: If you call Popen with shell=True and the length of PoShOutput
                     # exceeds the console's buffer width, the output will be split with
                     # extra, unexpected \r\n at the corresponding spots.
                     # NOTE 2: PoSh can return XML too if you need it.
-                    self.addToPSHistory(PSCommand)
+                    self._addToPSHistory(userPoShCmd)
                     # PS will insert \r\n at the end of every line--normalize.
-                    view.replace(region, PSOutput[:-2].replace('\r\n', '\n'))
+                    view.replace(region, PoShOutput[:-2].replace('\r\n', '\n'))
 
-        # Don't make the user retype the last unsuccessful command
-        # if it was a Powershell error.
         initialText = args[0] if args else self.lastFailedCommand
-        w = view.window()
-        w.showInputPanel("PS cmd:", initialText, onDone, None, None)
+        view.window().showInputPanel("PoSh cmd:", initialText, onDone, None, None)
+
+def getPoShSavedHistory():
+    # If the command history file doesn't exist now, it will be created when
+    # the user chooses to persist the current history for the first time.
+    try:
+        with open(getPathToPoShHistoryDB(), 'r') as f:
+            return [command[:-1].decode('utf-8') for command in f.readlines()]
+    except IOError:
+        return []
 
 def getDOSPromptDefaultCodepage():
     # Retrieve codepage with Win API call and use that.
@@ -140,7 +143,7 @@ def getDOSPromptDefaultCodepage():
     codepage = "cp" + codepage[:-2].split(" ")[-1:][0].strip()
     return codepage
 
-def getPoShCmdLine(script, argsToScript=""):
+def getPoShCmdLine(pathToScriptFile, argsToScript=""):
     return ["powershell",
                         "-noprofile",
                         "-nologo",
@@ -148,7 +151,7 @@ def getPoShCmdLine(script, argsToScript=""):
                         # PoSh 2.0 lets you specify an ExecutionPolicy
                         # from the cmdline, but 1.0 doesn't.
                         "-executionpolicy", "remotesigned",
-                        "-file", script,
+                        "-file", pathToScriptFile,
                         # According to Popen, CreateProcess doesn't allow strings containing
                         # nulls as parameters. Besides, PoSh will get confused if we pass
                         # UTF8 strings as args to the script. Do the following instead:
@@ -156,4 +159,3 @@ def getPoShCmdLine(script, argsToScript=""):
                         #   * Encode resulting bytestring in base64.
                         #   * Decode in the PoSh script.
                         base64.b64encode(argsToScript.encode("utf-16LE")),]
-
